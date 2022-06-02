fbec220e3364e42d1f3ba7bd59ae4957a110fe9b
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-06-02 07:00:24 +0100

    Setting up the project

--- src/App.js
+++ src/App.js
@@ -4,55 +4,57 @@ import IntroPage from "./components/IntroPage";
 import "./App.css";
 
 export default function App() {
-  const [quiz, setQuiz] = useState([])
-  useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5')
-    .then(res => res.json())
-    .then(data => {
-      // console.log(data.results)
-      setQuiz(data.results)
-      
-    })
-  }, [])
-  const [show, setShow] = useState(false)
-  function showGame() {
-    setShow(!show)
-  }
-  let hide= show ? "hide" : ""
-  let userAnswer = []
-  function getAnswer(e) {
-    // console.log(e.target.value)
-    // if(e.target.value === quiz[e.target.name].correct_answer) {
-    //     console.log("correct")
+    const [quiz, setQuiz] = useState([])
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5')
+        .then(res => res.json())
+        .then(data => {
+        // console.log(data.results)
+        setQuiz(data.results)
+        
+        })
+    }, [])
+    const [show, setShow] = useState(false)
+    function showGame() {
+        setShow(!show)
+    }
+    let hide= show ? "hide" : ""
+    let userAnswer = []
+    // function getAnswer(e) {
+    //     // console.log(e.target.value)
+    //     // if(e.target.value === quiz[e.target.name].correct_answer) {
+    //     //     console.log("correct")
+    //     // }
+    //     // userAnswer[index] = e.target.value
+    //     // console.log(userAnswer)
     // }
-    // userAnswer[index] = e.target.value
-    // console.log(userAnswer)
-  }
 
-  function getScore() {
-    let answerArray = quiz.map(each => each.correct_answer)
-    console.log(answerArray)
-    // let score = 0
-    // quiz.map(eachquiz => {
-    //   if(eachquiz.correct_answer === true) {
-    //       console.log(eachquiz.correct_answer)
-    //     score++
-    //   }
+    // function getScore() {
+    //     let answerArray = quiz.map(each => each.correct_answer)
+    //     console.log(answerArray)
+    //     // let score = 0
+    //     // quiz.map(eachquiz => {
+    //     //   if(eachquiz.correct_answer === true) {
+    //     //       console.log(eachquiz.correct_answer)
+    //     //     score++
+    //     //   }
 
-    // })
-    // return score
-  }
+    //     // })
+    //     // return score
+    // }
   
-  return (
-    
-    <div className="App">
-      <div className={`container ${hide}`}>
-      <IntroPage showGame={showGame}/>
-      {/* <p>{show ? "True" : "False"}</p> */}
+    return (
+        
+        <div className="App">
+            <div className={`container ${hide}`}>
+                <IntroPage showGame={showGame}/>
+            </div>
+            
+            
+            {show && <Game game={quiz}/>}
+
+                {/* {show && <Game getScore={getScore} getAnswer={getAnswer} game={quiz}/>} */}
+                {/* <Game /> */}
         </div>
-        {show && <Game game={quiz}/>}
-        {/* {show && <Game getScore={getScore} getAnswer={getAnswer} game={quiz}/>} */}
-        {/* <Game /> */}
-    </div>
-  );
+    );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,63 +1,35 @@
 import { useEffect, useState } from "react";
 
 export default function Game(props) {
-//   0: Object
-// 1: Object
-// 2: Object
-// 3: Object
-// category: "Politics"
-// type: "multiple"
-// difficulty: "medium"
-// question: "In United States history, how many vice presidents did Franklin D. Roosevelt have during his time in office as president?"
-// correct_answer: "3"
-// incorrect_answers: Array(3)
-// 0: "2"
-
-    // function getAnswer(e) {
-    //     // console.log(e.target.value)
-    //     if(e.target.value === props.game[e.target.name].correct_answer) {
-    //         console.log("correct")
-    //     }
-    // }
     let userAnswer = []
-    // let [score, setScore] = useState(0)
+    // const [score, setScore] = useState(0)
     let score = 0;
-    useEffect(() => {
-        let score = 0;
-    }, [])
-  function getAnswer(e, bindex) {
-    console.log(e.target.value)
-    // if(e.target.value === quiz[e.target.name].correct_answer) {
-    //     console.log("correct")
-    // }
-    userAnswer[bindex] = e.target.value
-    console.log(userAnswer)
-  }
+    // useEffect(() => {
+    //     let score = 0;
+    // }, [])
 
-  function getScore() {
-    let answerArray = props.game.map(each => each.correct_answer)
-    console.log(answerArray)
-    // let score = 0
-    for (let i = 0; i < answerArray.length; i++) {
-        if (answerArray[i] === userAnswer[i]) {
-        //   console.error(`Values at index ${index} do not match`);
-        //   return false;
-            // setScore(score + 1)
-            score = score + 1
+    function getAnswer(e, bindex) {
+        console.log(e.target.value)
+        // if(e.target.value === quiz[e.target.name].correct_answer) {
+        //     console.log("correct")
+        // }
+        userAnswer[bindex] = e.target.value
+        console.log(userAnswer)
+    }
+
+    function getScore() {
+        let answerArray = props.game.map(each => each.correct_answer)
+        console.log(answerArray)
+        // let score = 0
+        for (let i = 0; i < answerArray.length; i++) {
+            if (answerArray[i] === userAnswer[i]) {
+                score = score + 1
+            }
         }
-      }
-    // let score = 0
-    // quiz.map(eachquiz => {
-    //   if(eachquiz.correct_answer === true) {
-    //       console.log(eachquiz.correct_answer)
-    //     score++
-    //   }
-
-    // })
-    // return score
-    console.log(score)
-    return score
-  }
+    
+        console.log(score)
+        return score
+    }
   
   return (
     <div className="game">
@@ -67,12 +39,12 @@ export default function Game(props) {
                 
                 options.sort(() => Math.random() - 0.5);
                 
-                let allOptions = [
-                    {option: options[0], correct: false},
-                    {option: options[1], correct: false},
-                    {option: options[2], correct: false},
-                    {option: options[3], correct: true}
-                ]
+                // let allOptions = [
+                //     {option: options[0], correct: false},
+                //     {option: options[1], correct: false},
+                //     {option: options[2], correct: false},
+                //     {option: options[3], correct: true}
+                // ]
                 return (
                     <div className="question" key={bindex}>
                         <h3 dangerouslySetInnerHTML={{__html: eachquiz.question}}></h3>
